0280c3601019547bc7e2339c5b1f9fc7d9993c4e
Liciax <aboubouille@gmail.com>
2016-02-11 18:05:27 +0100

    changement lecture des donnees

--- Projet_Plateforme/src/principal/Plateforme.java
+++ Projet_Plateforme/src/principal/Plateforme.java
@@ -2,16 +2,25 @@ package principal;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.HashMap;
 
 public class Plateforme {
 	
-	public static void lectureDonnees() {
-		
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(""));
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+	private static HashMap<String, String> map;
+	
+//	public static void lectureDonnees() {
+//		map = new HashMap<String, String>();
+//		String ligne;
+//		
+//		try {
+//			BufferedReader br = new BufferedReader(new FileReader("donneesProduit.txt")); //chemin a définir
+//			while((ligne = br.readLine()) != null) {
+//				map.put(ligne.split("|")[0],ligne.split("|")[1]);
+//			}
+//			
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+	
 }
