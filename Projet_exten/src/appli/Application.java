fd501468cb72e0f8d205550fa0084658361f1f1e
Liciax <aboubouille@gmail.com>
2016-02-25 21:47:20 +0100

    affichage propre

--- Projet_exten/src/appli/Application.java
+++ Projet_exten/src/appli/Application.java
@@ -1,53 +1,45 @@
 package appli;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.util.Scanner;
 import java.util.Vector;
 
 import donnees.IProducteur;
-
 import afficheur.IAfficheur;
-
 import plateforme.Plateforme;
 
 public class Application {
-	
-	
 	
 	/**
 	 * 
 	 */
 	public Application() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	public void doIt() {
 		Vector<String> listeExtention = Plateforme.getPlateforme().getExtensions();
 		int i;
-		System.out.println(listeExtention.size());
+		System.out.println("Liste des extensions proposées :");
 		for (i = 0; i < listeExtention.size(); i++) {
-			System.out.println(listeExtention.get(i));
+			System.out.println(" - " + listeExtention.get(i));
 		}
+		System.out.println("--------------------------------");
 		Scanner sc = new Scanner(System.in);
-		System.out.println("quel affichage?");
+		System.out.println("Quel affichage ? ");
 		i = sc.nextInt();
 		IAfficheur aff = null;
 		try {
 			aff = (IAfficheur) Plateforme.getPlateforme().CreaInstance(listeExtention.get(i));
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		System.out.println("quel producteur?");
+		System.out.println("Quel producteur ? ");
 		i = sc.nextInt();
 		IProducteur prod = null;
 		try {
 			prod = (IProducteur) Plateforme.getPlateforme().CreaInstance(listeExtention.get(i));
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		aff.afficheMagasin(prod.getMagasin());
