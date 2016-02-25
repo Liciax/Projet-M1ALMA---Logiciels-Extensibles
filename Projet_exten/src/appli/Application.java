f7cb10c670543d351c1587ba6bc064526984ab1c
XyLnEn <lenny.lucas44@gmail.com>
2016-02-25 18:00:07 +0100

    modif plateforme

--- Projet_exten/src/appli/Application.java
+++ Projet_exten/src/appli/Application.java
@@ -1,5 +1,23 @@
 package appli;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Scanner;
+import java.util.Vector;
+
+import afficheur.IAfficheur;
+
+import plateforme.Plateforme;
+
 public class Application {
+	
+
+	public void doIt() {
+		Vector<String> listeExtention = Plateforme.getPlateforme().getExtension();
+		
+//		IAfficheur aff = (IAfficheur) Plateforme.getPlateforme().getIAfficheur();
+//		IProducteur prod = Plateforme.getPlateforme().getIProducteur();
+//		aff.afficheMagasin(prod.getMagasin());
+	}
 
 }
--- Projet_exten/src/plateforme/Plateforme.java
+++ Projet_exten/src/plateforme/Plateforme.java
@@ -8,11 +8,15 @@ import java.util.Vector;
 
 public class Plateforme {
 	
+	private Plateforme(){
+		
+	}
+	private static Plateforme plateforme;
+	
 	public Class loadAppli() {
 		BufferedReader br;
 		int i;
 		String templigne;
-//		Vector<Class> listeClass = new Vector<Class>();
 		Vector<String> listeClass = new Vector<String>();
 		Class app;
 		//on va lire le fichier
@@ -30,7 +34,7 @@ public class Plateforme {
 			Scanner sc = new Scanner(System.in);
 			System.out.println("lequel?");
 			i = sc.nextInt();
-			app = (Class) Class.forName(listeClass.get(i-1));
+			app = (Class) Class.forName(listeClass.get(i));
 		} catch (Exception e) {
 			app = null;
 		}
@@ -44,6 +48,92 @@ public class Plateforme {
 		m1.invoke(obj, null);
 	}
 	
+	public Vector<String> getExtension() {
+		BufferedReader br;
+		int i;
+		String templigne;
+//		Vector<Class> listeClass = new Vector<Class>();
+		Vector<String> listeClass = new Vector<String>();
+		Object classe;
+		try {
+			//lecture...
+			br = new BufferedReader(new FileReader("nodeAfficheur.txt"));
+			while((templigne= br.readLine()) != null) {
+				//cas ou la ligne n'est pas un commentaire
+				if(!templigne.split(":")[0].equals("#")) {
+					listeClass.add((templigne.split(";")[0]).split("=")[1]);
+				}
+				
+			}
+		} catch (Exception e) {
+			classe = null;
+		}
+		return listeClass;
+	}
+	
+	public Object CreaInstance(String nomClasse) throws Exception{
+		return Class.forName(nomClasse).newInstance();
+	}
+	
+	public Object getIAfficheur() {
+		
+		BufferedReader br;
+		int i;
+		String templigne;
+//		Vector<Class> listeClass = new Vector<Class>();
+		Vector<String> listeClass = new Vector<String>();
+		Object classe;
+		try {
+			//lecture...
+			br = new BufferedReader(new FileReader("nodeAfficheur.txt"));
+			while((templigne= br.readLine()) != null) {
+				//cas ou la ligne n'est pas un commentaire
+				if(!templigne.split(":")[0].equals("#")) {
+					listeClass.add((templigne.split(";")[0]).split("=")[1]);
+				}
+				
+			}
+			Scanner sc = new Scanner(System.in);
+			System.out.println("l'afficheur?");
+			i = sc.nextInt();
+			classe = Class.forName(listeClass.get(i)).newInstance();
+		} catch (Exception e) {
+			classe = null;
+		}
+		return classe;
+		
+	}
+	
+	public Object getIProducteur() {
+		BufferedReader br;
+		int i;
+		String templigne;
+//		Vector<Class> listeClass = new Vector<Class>();
+		Vector<String> listeClass = new Vector<String>();
+		Object classe;
+		//on va lire le fichier
+		try {
+			//lecture...
+			br = new BufferedReader(new FileReader("nodeProducteur.txt"));
+			while((templigne= br.readLine()) != null) {
+				//cas ou la ligne n'est pas un commentaire
+				if(!templigne.split(":")[0].equals("#")) {
+					listeClass.add((templigne.split(";")[0]).split("=")[1]);
+				}
+				
+			}
+			Scanner sc = new Scanner(System.in);
+			System.out.println("le producteur?");
+			i = sc.nextInt();
+			classe = Class.forName(listeClass.get(i)).newInstance();
+		} catch (Exception e) {
+			classe = null;
+		}
+		return classe;
+	}
+	
+	
+	
 	public void main(String args[]) {
 		try {
 			makeClassDoIt(loadAppli());
@@ -51,6 +141,13 @@ public class Plateforme {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	public static  Plateforme getPlateforme() {
+		if(plateforme == null) {
+			plateforme = new Plateforme();
+		}
+		return plateforme;
 	}
 
 }
