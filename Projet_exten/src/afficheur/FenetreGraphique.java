33b052c9b6c30973911bdf9a5fad5404be373721
kirapoetica974 <retco.contact@gmail.com>
2016-02-29 18:32:39 +0100

    modif de afficheur graphique

--- Projet_exten/src/afficheur/FenetreGraphique.java
+++ Projet_exten/src/afficheur/FenetreGraphique.java
@@ -1,5 +1,40 @@
 package afficheur;
 
-public class FenetreGraphique {
+import java.awt.FlowLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import donnees.Magasin;
+import donnees.MagasinConcret;
+
+public class FenetreGraphique extends JFrame{
+
+	public FenetreGraphique(Magasin m) {
+		super("Application");
+
+		WindowListener l = new WindowAdapter() {
+			public void windowClosing(WindowEvent e){
+				System.exit(0);
+				}
+		};
+		addWindowListener(l);
+		setSize(400,300);
+		setVisible(true);
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(new FlowLayout());
+		
+		
+		JLabel label = new JLabel("Bienvenue dans ma modeste application");
+		 
+		panel.add(label);
+		
+		setContentPane(panel);
+	}
+	
 }
