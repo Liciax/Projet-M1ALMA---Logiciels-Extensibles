df46d9e2b16e0bb7f310a967461f9d7425ebdac0
kirapoetica974 <retco.contact@gmail.com>
2016-02-29 20:51:34 +0100

    modif de afficheur graphique : les produits s'affichent l'un en dessous de l'autre

--- Projet_exten/src/afficheur/AfficheurGraphique.java
+++ Projet_exten/src/afficheur/AfficheurGraphique.java
@@ -1,11 +1,15 @@
 package afficheur;
 
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.Panel;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
@@ -21,48 +25,56 @@ public class AfficheurGraphique implements IAfficheur{
 	public void afficheProduit(Produit p) {
 	}
 	
-	public Box afficheProduitGraphique(Produit p) {
+	public Panel afficheProduitGraphique(Produit p) {
+		//Creation de 3 labels
 		JLabel nom = new JLabel("Nom : " + p.getNom());
 		JLabel type = new JLabel("Type : " + p.getType());
 		JLabel prix = new JLabel("Prix : " + p.getPrix());
 		
-		Box hbox = Box.createHorizontalBox();
-		hbox.add(nom);
-		hbox.add(type);
-		hbox.add(prix);
+		nom.setBorder(BorderFactory.createEmptyBorder(0, 20, 0, 0));
+		type.setBorder(BorderFactory.createEmptyBorder(0, 20, 0, 0));
+		prix.setBorder(BorderFactory.createEmptyBorder(0, 20, 0, 0));
 		
-		return hbox;
+		// insertion des boutons dans une gridLayout
+		Panel pan = new Panel();
+		GridLayout gl = new GridLayout(3, 1);
+		pan.setLayout(gl);
+		pan.add("nom", nom);
+		pan.add("type",type);
+		pan.add("Prix",prix);
+		
+		return pan;
 	}
 	
 	public void afficheProduit(Produit p, JFrame f){
-		JPanel panel = new JPanel();
-		BoxLayout bl=new BoxLayout(panel,BoxLayout.Y_AXIS);
-		panel.setLayout(bl);
-		
-		
-		f.setContentPane(panel);		
+	
 	}
 
 	@Override
 	public void afficheMagasin(Magasin mag) {
 		JFrame frame = new JFrame("Aplication");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(400,300);
+		frame.setSize(300,600);
 		
 		
-		JLabel label = new JLabel("Bienvenue dans ma modeste application");
+		JLabel label = new JLabel("Bienvenue au magasin "+mag.getNomMag());
+		label.setBorder(BorderFactory.createEmptyBorder(0, 20, 0, 0));
+		
+		GridLayout gl = new GridLayout(mag.getProduits().size()+1, 1);
+		gl.setVgap(10); 
+		frame.setLayout(gl);
 		
 		
-		Box hbox = Box.createHorizontalBox();
-		hbox.add(label);
+		//Box hbox = Box.createVerticalBox();
+		frame.getContentPane().add(label);
 		
 		for (int i = 0; i < mag.getProduits().size(); i++) {
-			Box b = afficheProduitGraphique(mag.getProduits().get(i));
-			hbox.add(b);
+			Panel b = afficheProduitGraphique(mag.getProduits().get(i));
+			frame.getContentPane().add(b);
 		}
 		
-		Container contentpane = frame.getContentPane();
-		contentpane.add(hbox);
+		//Container contentpane = frame.getContentPane();
+		//contentpane.add(hbox);
 		
 		
 		
