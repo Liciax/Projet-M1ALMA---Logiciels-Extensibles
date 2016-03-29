9711a01e9d5bb9187fcd43c9f6dab212e4c5eff4
XyLnEn <lenny.lucas44@gmail.com>
2016-03-29 17:05:40 +0200

    good enough

--- Projet_exten/src/actuateur/MagasinHandlerNormal.java
+++ Projet_exten/src/actuateur/MagasinHandlerNormal.java
@@ -6,29 +6,80 @@ import donnees.IProduit;
 public class MagasinHandlerNormal implements IMagasinHandler {
 
 	public void ajouter(IProduit produit, IMagasin magasin, int quantite) {
-
+		boolean existant = false;
+		IProduit prod = null;
+		try {
+			prod = produit.getClass().newInstance();
+			prod.setNom(produit.getNom());
+			prod.setPrix(produit.getPrix());
+			prod.setType(produit.getType());
+		} catch (InstantiationException | IllegalAccessException e) {
+			e.printStackTrace();
+		}
 		for(IProduit p: magasin.getProduits()){
 			if(produit.getNom().equals(p.getNom())) {
-				IProduit nouvProd = null;
-				
-				try {
-					nouvProd = produit.getClass().newInstance();
-					nouvProd.setNom(produit.getNom());
-					nouvProd.setPrix(produit.getPrix());
-					nouvProd.setType(produit.getType());
-					nouvProd.setQuantites(quantite);
-				} catch (InstantiationException | IllegalAccessException e) {
-					e.printStackTrace();
-				}
-			
-				if(!(magasin.getProduits().contains(produit))) {
-					magasin.getProduits().add(nouvProd);
-				}else {
-					modifier(nouvProd, magasin, quantite);
-				}
-				
+				modifier(prod, magasin, quantite);//existe deja dans panier
+				existant = true;
 			}
 		}
+		if(!existant){
+			prod.setQuantites(quantite);
+			magasin.getProduits().add(prod);
+		}
+		/*
+		
+		
+		IProduit prod = null;
+		try {
+			prod = produit.getClass().newInstance();
+			prod.setNom(produit.getNom());
+			prod.setPrix(produit.getPrix());
+			prod.setType(produit.getType());
+		} catch (InstantiationException | IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		
+		for(IProduit p: magasin.getProduits()){
+			if(produit.getNom().equals(p.getNom())) {
+				pasExistant = false;
+				boolean trouve = false;
+				for(IProduit q: magasin.getProduits()){
+					if(produit.getNom().equals(q.getNom())) {
+						modifier(prod, magasin, quantite);//existe deja dans panier
+						trouve = true;
+					}
+					
+				}
+				if(!trouve) {
+					prod.setQuantites(quantite);
+					magasin.getProduits().add(prod);
+				}
+			}
+		}
+		
+//		for(IProduit p: magasin.getProduits()){
+//			if(produit.getNom().equals(p.getNom())) {
+//				IProduit nouvProd = null;
+//				
+//				try {
+//					nouvProd = produit.getClass().newInstance();
+//					nouvProd.setNom(produit.getNom());
+//					nouvProd.setPrix(produit.getPrix());
+//					nouvProd.setType(produit.getType());
+//					nouvProd.setQuantites(quantite);
+//				} catch (InstantiationException | IllegalAccessException e) {
+//					e.printStackTrace();
+//				}
+//			
+//				if(!(magasin.getProduits().contains(produit))) {
+//					magasin.getProduits().add(nouvProd);
+//				}else {
+//					modifier(nouvProd, magasin, quantite);
+//				}
+//				
+//			}
+//		}
+*/
 	}
 		
 	
