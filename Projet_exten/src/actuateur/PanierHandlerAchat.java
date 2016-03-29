c7ed9ed1d3d158c272878263a7c02969e170ba25
Liciax <aboubouille@gmail.com>
2016-03-29 14:10:59 +0200

    modif fonction valide pour panier

--- Projet_exten/src/actuateur/PanierHandlerAchat.java
+++ Projet_exten/src/actuateur/PanierHandlerAchat.java
@@ -22,15 +22,30 @@ public class PanierHandlerAchat implements IPanierHandler {
 			if(produit.getNom().equals(p.getNom())) {
 				p.setQuantites(produit.getQuantites() + quantite);
 			}
-		}     
+		}
 	}
 	
 	// valide simule un achat, donc vidage du contenu du panier
 	public boolean valider(IMagasin magasin) {
 		int prix = 0;
-		for(IProduit p: magasin.getPanier().getContenu()){
-			prix += p.getPrix() * p.getQuantites();
+		for(IProduit proPan: magasin.getPanier().getContenu()){
+			for (IProduit proMag : magasin.getProduits()) {
+				
+				if(proMag.getNom().equals(proPan.getNom())) {
+					if(proMag.getQuantites() >= proPan.getQuantites()) { // si assez de quantites en magasin
+						prix += proPan.getPrix() * proPan.getQuantites();
+						proMag.setQuantites(proMag.getQuantites() - proPan.getQuantites());
+						magasin.getPanier().getContenu().remove(proPan);
+					} else { // sinon manque de stock dans magasin
+						int reste = Math.abs(proMag.getQuantites() - proPan.getQuantites());
+						prix += proPan.getPrix() * (proPan.getQuantites() - reste);	
+						proMag.setQuantites(0);
+						modifier(proPan, magasin, reste);
+					}
+				}
+			}
 		}
+	
 		System.out.println("Vous allez devoir payer : " + prix + " €.");
 		return true;
 	}
--- Projet_exten/src/plateforme/Plateforme.java
+++ Projet_exten/src/plateforme/Plateforme.java
@@ -11,7 +11,6 @@ import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Vector;
 
 public class Plateforme {
 	/*
