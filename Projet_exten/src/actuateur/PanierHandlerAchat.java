b92df411a8b81909358e3c77924268d12a028f6c
XyLnEn <lenny.lucas44@gmail.com>
2016-03-29 16:13:23 +0200

    fix modif panierAchat

--- Projet_exten/src/actuateur/PanierHandlerAchat.java
+++ Projet_exten/src/actuateur/PanierHandlerAchat.java
@@ -6,24 +6,7 @@ import donnees.IProduit;
 public class PanierHandlerAchat implements IPanierHandler {
 
 	// ajoute au panier un nombre de produits disponible dans le magasin
-	public void ajouter(IProduit produit, IMagasin magasin, int quantite) {
-		
-//		if(!(magasin.getPanier().getContenu().contains(produit))) {
-//			IProduit nouvProd = null;
-//			try {
-//				nouvProd = produit.getClass().newInstance();
-//				nouvProd.setNom(produit.getNom());
-//				nouvProd.setPrix(produit.getPrix());
-//				nouvProd.setType(produit.getType());
-//				nouvProd.setQuantites(quantite);
-//			} catch (InstantiationException | IllegalAccessException e) {
-//				e.printStackTrace();
-//			}
-//			magasin.getPanier().getContenu().add(nouvProd);
-//		}else {
-//			modifier(produit, magasin, quantite);
-//		}
-		
+	public void ajouter(IProduit produit, IMagasin magasin, int quantite) {		
 		for(IProduit p: magasin.getProduits()){
 			if(produit.getNom().equals(p.getNom())) {
 				IProduit prod = null;
@@ -38,7 +21,6 @@ public class PanierHandlerAchat implements IPanierHandler {
 				boolean trouve = false;
 				for(IProduit q: magasin.getPanier().getContenu()){
 					if(produit.getNom().equals(q.getNom())) {
-						System.out.println("passé par la");
 						modifier(prod, magasin, quantite);//existe deja dans panier
 						trouve = true;
 					}
@@ -64,7 +46,7 @@ public class PanierHandlerAchat implements IPanierHandler {
 	
 	// valide simule un achat, donc vidage du contenu du panier
 	public boolean valider(IMagasin magasin) {
-		int prix = 0;
+		float prix = 0;
 		for(IProduit proPan: magasin.getPanier().getContenu()){
 			for (IProduit proMag : magasin.getProduits()) {
 				if(proMag.getNom().equals(proPan.getNom())) {
@@ -75,26 +57,15 @@ public class PanierHandlerAchat implements IPanierHandler {
 			}
 		}
 		//quantité dans panier ok, on peut modif magasin
-		for(IProduit proPan: magasin.getPanier().getContenu()){
-			for (IProduit proMag : magasin.getProduits()) {
-				
-				if(proMag.getNom().equals(proPan.getNom())) {
-					if(proMag.getQuantites() >= proPan.getQuantites()) { // si assez de quantites en magasin
-						prix += proPan.getPrix() * proPan.getQuantites();
-						proMag.setQuantites(proMag.getQuantites() - proPan.getQuantites());
-						magasin.getPanier().getContenu().remove(proPan);
-					} else { // sinon manque de stock dans magasin
-						int reste = Math.abs(proMag.getQuantites() - proPan.getQuantites());
-						prix += proPan.getPrix() * (proPan.getQuantites() - reste);	
-						proMag.setQuantites(0);
-						proPan.setQuantites(reste);
-						//modifier(proPan, magasin, reste);
-						return false;
-					}
+		for(IProduit proPan2: magasin.getPanier().getContenu()){
+			for (IProduit proMag2 : magasin.getProduits()) {
+				if(proMag2.getNom().equals(proPan2.getNom())) {
+					prix += proPan2.getPrix() * proPan2.getQuantites();
+					proMag2.setQuantites(proMag2.getQuantites() - proPan2.getQuantites());						
 				}
 			}
 		}
-	
+		magasin.getPanier().getContenu().clear();
 		System.out.println("Vous allez devoir payer : " + prix + " €.");
 		return true;
 	}
