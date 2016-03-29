0728cfb465da0752032ac3cd274ea1e59aa190e3
Liciax <aboubouille@gmail.com>
2016-03-29 16:04:35 +0200

    magasin handler normal

--- Projet_exten/src/actuateur/MagasinHandlerNormal.java
+++ Projet_exten/src/actuateur/MagasinHandlerNormal.java
@@ -6,49 +6,38 @@ import donnees.IProduit;
 public class MagasinHandlerNormal implements IMagasinHandler {
 
 	public void ajouter(IProduit produit, IMagasin magasin, int quantite) {
-		if(!(magasin.getProduits().contains(produit))) {
-			IProduit nouvProd = null;
-			try {
-				nouvProd = produit.getClass().newInstance();
-				nouvProd.setNom(produit.getNom());
-				nouvProd.setPrix(produit.getPrix());
-				nouvProd.setType(produit.getType());
-				nouvProd.setQuantites(quantite);
-			} catch (InstantiationException | IllegalAccessException e) {
-				e.printStackTrace();
+
+		for(IProduit p: magasin.getProduits()){
+			if(produit.getNom().equals(p.getNom())) {
+				IProduit nouvProd = null;
+				
+				try {
+					nouvProd = produit.getClass().newInstance();
+					nouvProd.setNom(produit.getNom());
+					nouvProd.setPrix(produit.getPrix());
+					nouvProd.setType(produit.getType());
+					nouvProd.setQuantites(quantite);
+				} catch (InstantiationException | IllegalAccessException e) {
+					e.printStackTrace();
+				}
+			
+				if(!(magasin.getProduits().contains(produit))) {
+					magasin.getProduits().add(nouvProd);
+				}else {
+					modifier(nouvProd, magasin, quantite);
+				}
+				
 			}
-			magasin.getProduits().add(nouvProd);
-		}else {
-			modifier(produit, magasin, quantite);
 		}
 	}
-//		for(IProduit p: magasin.getProduits()){
-//			if(produit.getNom().equals(p.getNom())) {
-//				IProduit nouvProd = null;
-//				
-//				try {
-//					nouvProd = produit.getClass().newInstance();
-//					nouvProd.setNom(produit.getNom());
-//					nouvProd.setPrix(produit.getPrix());
-//					nouvProd.setType(produit.getType());
-//					nouvProd.setQuantites(quantite);
-//				} catch (InstantiationException | IllegalAccessException e) {
-//					e.printStackTrace();
-//				}
-//			
-//				if(!(magasin.getProduits().contains(produit))) {
-//					magasin.getProduits().add(nouvProd);
-//				}else {
-//					modifier(nouvProd, magasin, quantite);
-//				}
-//				
-//			}
-//		}
-
 		
 	
-	public void modifier(IProduit p, IMagasin magasin, int quantite) {
-		
+	public void modifier(IProduit produit, IMagasin magasin, int quantite) {
+		for(IProduit p: magasin.getProduits()){
+			if(produit.getNom().equals(p.getNom())) {
+				p.setQuantites(produit.getQuantites() + quantite);
+			}
+		}
 	}
 
 	
