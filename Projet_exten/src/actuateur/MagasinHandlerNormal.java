bc56dfd277c3ce1a33180c54eaaecb56ef7ea7d6
Liciax <aboubouille@gmail.com>
2016-03-29 14:12:24 +0200

    ajout magasinHandlerNormal

---
+++ Projet_exten/src/actuateur/MagasinHandlerNormal.java
@@ -0,0 +1,22 @@
+package actuateur;
+
+import donnees.IMagasin;
+import donnees.IProduit;
+
+public class MagasinHandlerNormal implements IMagasinHandler {
+
+	public void ajouter(IProduit p, IMagasin magasin, int quantite) {
+		
+	}
+	
+	
+	public void modifier(IProduit p, IMagasin magasin, int quantite) {
+		
+	}
+
+	
+	public void supprimer(IProduit p, IMagasin magasin) {
+		
+	}
+
+}
