cefbc8bbaa785d3a6bb633e01af4e75a233a8ef6
Liciax <aboubouille@gmail.com>
2016-03-30 15:29:18 +0200

    test de compression des jar dans le script

--- Projet_exten/install.sh
+++ Projet_exten/install.sh
@@ -3,11 +3,15 @@
 THE_CLASSPATH=
 PROGRAM_NAME=plateforme/Plateforme.java
 cd src
+jar cf appli.jar -C appli/* ./appli
+
 for i in `ls ./*.jar`
   do
   THE_CLASSPATH=${THE_CLASSPATH}:${i}
   echo $THE_CLASSPATH
 done
+
+#find . -name *.java | xargs javac -classpath ".:${THE_CLASSPATH}"
 
 javac -classpath ".:${THE_CLASSPATH}" donnees/*.java
 javac -classpath ".:${THE_CLASSPATH}" appli/*.java
